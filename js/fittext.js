f72c0e628bb1f4029c6e5b99a8f672cff865ff29
tedsilb <26172399+tedsilb@users.noreply.github.com>
2022-02-28 06:34:19 +0000

    Deploying to gh-pages from @ tedsilb/personal-site@f7b411da1ad651312ec335c85df912343eb06ba9 🚀

--- js/fittext.js
+++ js/fittext.js
@@ -2,20 +2,16 @@
  * Fit text to the width of the page by adjusting font size.
  *
  * @param {HTMLElement} element Element containing text to fit.
- * @param {{minFontSize: string, maxFontSize: string}} options Options for
- *     fitting text.
+ * @param {{minFontSize: string, maxFontSize: string}} options Options for fitting text.
  */
 export function fitText(element, options) {
   const compressor = options.compressor || 1;
-  const minFontSize =
-      parseFloat(options.minFontSize) || Number.NEGATIVE_INFINITY;
-  const maxFontSize =
-      parseFloat(options.maxFontSize) || Number.POSITIVE_INFINITY;
+  const minFontSize = parseFloat(options.minFontSize) || Number.NEGATIVE_INFINITY;
+  const maxFontSize = parseFloat(options.maxFontSize) || Number.POSITIVE_INFINITY;
 
   const resize = () => {
-    const newFontSize = Math.max(
-        Math.min(element.offsetWidth / (compressor * 10), maxFontSize),
-        minFontSize);
+    const newFontSize =
+        Math.max(Math.min(element.offsetWidth / (compressor * 10), maxFontSize), minFontSize);
     element.style.fontSize = `${newFontSize}px`;
   };
 
